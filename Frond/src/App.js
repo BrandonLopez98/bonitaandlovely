f23465a2b8e804e818f9659edc29fa0ca9a57620
ivethglez80 <ivethglez80@gmail.com>
2023-08-11 14:24:36 -0300

    updating

--- Frond/src/App.js
+++ Frond/src/App.js
@@ -21,7 +21,7 @@ import axios from "axios"
 import Navbar from "./components/NavBar/NavBar";
 import Footer from "./components/Footer/Footer";
 import AddToCart from "./views/Cart/AddToCart";
-import Carrito from "./views/Cart/Carrito";
+
 import { useParams } from "react-router-dom"; 
 //para no repetir el puerto:(se está configurando una URL base que se utilizará como prefijo para todas las peticiones realizadas con Axios) 
 axios.defaults.baseURL = "http://localhost:3001/"
