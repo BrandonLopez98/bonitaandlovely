825b1f3e95315579f58057540c6f417e510bf414
ivethglez80 <ivethglez80@gmail.com>
2023-08-16 12:48:30 -0300

    my local complete

--- Frond/src/redux/reduce.js
+++ Frond/src/redux/reduce.js
@@ -24,46 +24,46 @@ const InitialState = {
     localCart: initialLocalCart
 }
 
-const reducer = (state = InitialState, {type, payload, data}) => {
+const reducer = (state = InitialState, { type, payload, data }) => {
     switch (type) {
-        case ALLPRODUCTS :
-            return{
+        case ALLPRODUCTS:
+            return {
                 ...state,
                 Allproducts: payload
             }
         case PRODUCTOS:
-            return{
+            return {
                 ...state,
                 productos: payload
             }
         //case provisional
         case COPY_ALLPRODUCTS:
-            return{
+            return {
                 ...state,
                 copyAllProducts: payload
             }
-        case ALLCATEGORIES :
-            return{
+        case ALLCATEGORIES:
+            return {
                 ...state,
                 Allcategories: payload
             }
-        case ALLSUBCATEGORIES :
-            return{
+        case ALLSUBCATEGORIES:
+            return {
                 ...state,
                 Allsubcategories: payload
             }
-        case ALLBRANDS :
-            return{
-                 ...state,
+        case ALLBRANDS:
+            return {
+                ...state,
                 Allbrands: payload
             }
-        case ALLCOLORS :
-            return{
+        case ALLCOLORS:
+            return {
                 ...state,
                 Allcolors: payload
             }
-        case ALLSIZES :
-            return{
+        case ALLSIZES:
+            return {
                 ...state,
                 Allsizes: payload
             }
@@ -72,33 +72,33 @@ const reducer = (state = InitialState, {type, payload, data}) => {
                 ...state,
                 productsDetail: payload
             };
-    
+
         case CLEAN_DETAIL:
             return {
                 ...state,
-               productsDetail: []
+                productsDetail: []
             }
         case POST_FAVORITES_API:
-            const productosFav = (id) =>{
-                return state.productos.find((product)=>product.id===id)
+            const productosFav = (id) => {
+                return state.productos.find((product) => product.id === id)
             }
-            const {productoId} = payload
+            const { productoId } = payload
             const newFavorites = [...state.favorites, productosFav(`prod-${productoId}`)]
             return {
                 ...state,
                 favorites: newFavorites,
                 favoritesRaw: data
             }
-        
+
         case POST_FAVORITES_API_INICIO:
             const productsFavI = (ids) => {
                 return state.productos.filter((prod) => ids.includes(prod.id));
-              };
+            };
 
-              const favoritesInicio = productsFavI(payload);
+            const favoritesInicio = productsFavI(payload);
             return {
                 ...state,
-                favorites: favoritesInicio, 
+                favorites: favoritesInicio,
                 favoritesRaw: data
             }
 
@@ -106,12 +106,12 @@ const reducer = (state = InitialState, {type, payload, data}) => {
             const productsFav = (id) => {
                 return state.productos.find((prod) => prod.id === id);
             };
-            
+
             if (payload.length > 0) {
                 const newLocalFavorites = [...state.localFavorites, productsFav(payload)];
                 // Actualiza el Local Storage con la nueva lista de favoritos
                 localStorage.setItem("localFavorites", JSON.stringify(newLocalFavorites));
-    
+
                 return {
                     ...state,
                     localFavorites: newLocalFavorites,
@@ -127,10 +127,10 @@ const reducer = (state = InitialState, {type, payload, data}) => {
             const newLocalFavoritesAfterDelete = state.localFavorites.filter((item) => item.id !== payload);
             // Actualiza el Local Storage con la nueva lista de favoritos después de eliminar
             localStorage.setItem("localFavorites", JSON.stringify(newLocalFavoritesAfterDelete));
-      
+
             return {
-              ...state,
-              localFavorites: newLocalFavoritesAfterDelete,
+                ...state,
+                localFavorites: newLocalFavoritesAfterDelete,
             };
         case DELETE_FAVORITES_API:
             const newFavoritesAfterDelete = state.favorites.filter((item) => item.id !== payload);
@@ -139,7 +139,7 @@ const reducer = (state = InitialState, {type, payload, data}) => {
                 favorites: newFavoritesAfterDelete,
                 favoritesRaw: data
             };
-                
+
         case PRODUCTS_FILTERED:
             const filtrarProductos = (productos, filtro) => {
                 if (
@@ -147,69 +147,70 @@ const reducer = (state = InitialState, {type, payload, data}) => {
                     filtro.marcaId.length === 0 &&
                     (!filtro.precio_venta.min && !filtro.precio_venta.max) &&
                     filtro.tamañoId.length === 0
-                  ) {
+                ) {
                     return []; // Devuelve un array vacío si no hay filtros
-                  } else {
-                      return productos.filter((producto) => {
+                } else {
+                    return productos.filter((producto) => {
                         if (filtro.categoriaId && filtro.categoriaId.length > 0) {
-                          if (!filtro.categoriaId.includes(producto.categoriaId)) {
-                            return false;
-                          }
+                            if (!filtro.categoriaId.includes(producto.categoriaId)) {
+                                return false;
+                            }
                         }
                         if (filtro.marcaId && filtro.marcaId.length > 0) {
-                          if (!filtro.marcaId.includes(producto.marcaId)) {
-                            return false;
-                          }
+                            if (!filtro.marcaId.includes(producto.marcaId)) {
+                                return false;
+                            }
                         }
-                    
+
                         if (filtro.precio_venta && filtro.precio_venta.min && filtro.precio_venta.max) {
-                          const precioVenta = parseFloat(producto.precio_venta);
-                          if (precioVenta < filtro.precio_venta.min || precioVenta > filtro.precio_venta.max) {
-                            return false;
-                          }
+                            const precioVenta = parseFloat(producto.precio_venta);
+                            if (precioVenta < filtro.precio_venta.min || precioVenta > filtro.precio_venta.max) {
+                                return false;
+                            }
                         }
-                    
+
                         if (filtro.tamañoId && filtro.tamañoId.length > 0) {
-                          if (!filtro.tamañoId.includes(producto.tamañoId)) {
-                            return false;
-                          }
+                            if (!filtro.tamañoId.includes(producto.tamañoId)) {
+                                return false;
+                            }
                         }
                         return true;
-                      });
-                  }
-              };
-              const productosFiltrados = filtrarProductos(state.productos, payload);
+                    });
+                }
+            };
+            const productosFiltrados = filtrarProductos(state.productos, payload);
             return {
                 ...state,
                 productsFiltered: productosFiltrados
             };
 
-            case CART_PRODUCTS:
-                return {
-                    ...state,
-                   cartProducts: payload
-                };
-    
+        case CART_PRODUCTS:
+            return {
+                ...state,
+                cartProducts: payload
+            };
 
-            case ADD_TO_CART:
-                return {
-                    ...state,
-                    addProductsToCart: payload
-                };
 
-            case GETPRODUCT_BYNAME:
-                return {
-                    ...state,
-                    searchResults: payload
-                }
+        case ADD_TO_CART:
+            return {
+                ...state,
+                addProductsToCart: payload
+            };
 
-                case POST_CART_LS:
+        case GETPRODUCT_BYNAME:
+            return {
+                ...state,
+                searchResults: payload
+            }
+
+            case POST_CART_LS:
                 const itemsInCart = (id) =>{
                     return state.productos.find((prod)=>prod.id===id);
                 };
-                const { id, amount } = payload;                 
+                const { id, amount, color } = payload;  
+                console.log("color en reduce",color);               
                 if (amount>0){                                                           
-                        const newItem = {...itemsInCart(id), amount}                                
+                        const newItem = {...itemsInCart(id), color, amount}                                
                         const newItemsInCart = [...state.localCart, newItem];                                
                         localStorage.setItem("localCart", JSON.stringify(newItemsInCart));                                                 
                         return{
@@ -222,17 +223,19 @@ const reducer = (state = InitialState, {type, payload, data}) => {
                         localCart:[]
                     }
                 }
-                
 
-            case EMPTY_LOCAL_CART:
-                localStorage.removeItem("localCart");
-                return {
-                    ...state,
-                    localCart:[]
+
+
+
+        case EMPTY_LOCAL_CART:
+            localStorage.removeItem("localCart");
+            return {
+                ...state,
+                localCart: []
             };
-    
+
         default:
-        return state
+            return state
     }
 }
 
--- Frond/src/views/Cart/Carrito.jsx
+++ Frond/src/views/Cart/Carrito.jsx
@@ -2,54 +2,61 @@ import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import {emptyCartLS} from "../../redux/actions"
 import { NavLink } from 'react-router-dom';
-
-// const totalItemsCart = [1];
-// const cantidad = 1;
-
+import { useAuth0 } from "@auth0/auth0-react";
 
 const Carrito = () => {
     const dispatch = useDispatch();
     
     const cartLS = useSelector(state => state.localCart); //estos son los item en carrito en local/
+    // const { user, isAuthenticated} = useAuth0();
+    const user={id:2}
+
+    console.log("cantidad de objetos en cartLs", cartLS.length);
     cartLS.forEach(elem=>{
-        if (elem !== undefined && elem !== null){
-        console.log("cartLS",elem.name);        
+        console.log("asi llega local cart a carrito");
+        console.log(elem.id);
+        console.log(elem.color);        
+        console.log(elem.precio_venta);
         console.log(elem.amount);
-    }})
-    
+    })
+
+
+
     const handleEmptyCart = () =>{
         dispatch(emptyCartLS());
     }        
     
    /* unificar amount de articulos start*/
-   const cartUnif = (cart) =>{
+   const cartUnif = (cart) => {
     const countMap = {};
-    const validItems = cart.filter(item => item !== undefined && item !== null);
- 
-     cart.forEach(item=>{
-        if (item.id !== undefined && item.id !== null)  
-        {         
-         const itemId=item.id;       
-         if(countMap[itemId]){
-             countMap[itemId]+=item.amount;
-         }else{
-             countMap[itemId]=item.amount;
-         }             
-     }});     
-     const cartUnifRes = Object.keys(countMap).filter((itemId) => {
-        return cart.find((item) => item.id === itemId) !== undefined;
-      })
-        .map((itemId) => {
-          return {
-            objeto: cart.find((item) => item.id === itemId),
-            cantidad: countMap[itemId],
-          };
-        });    
-      return cartUnifRes;
-    };
+  
+    cart.forEach((item) => {
+      if (item.id !== undefined && item.id !== null && item.color !== undefined && item.color !== null) {
+        const itemKey = `${item.id}_${item.color}`;
+        if (countMap[itemKey]) {
+          countMap[itemKey] += item.amount;
+        } else {
+          countMap[itemKey] = item.amount;
+        }
+      }
+    });
+  
+    const cartUnifRes = Object.keys(countMap).map((itemKey) => {
+      const [itemId, color] = itemKey.split('_');
+      return {
+        objeto: cart.find((item) => item.id === itemId && item.color === color),
+        cantidad: countMap[itemKey],
+        color: color,
+      };
+    });
+  
+    return cartUnifRes;
+  };
+  
 /* unificar amount de articulos end*/
 
-    const cartUnificado = cartUnif(cartLS);    
+    const cartUnificado = cartUnif(cartLS); 
+    // dispatch(addCartLSToApi(cartUnificado, user.id));   
 
 
    /* total costo x articulos */
@@ -57,6 +64,17 @@ const Carrito = () => {
 
    /* total de articulos en carrito local*/
     const totalArts = cartUnificado.reduce((qty,item)=>qty+(item.cantidad),0);
+
+    // const goPay = () =>{
+    //     const cartToPay = `/carrito-${clienteId}`
+    // }
+
+    const handleProceedToPayment = () => {
+        
+
+        // axios.post('http://localhost:3001/pago', productToPay)
+        //     .then((res) => (window.location.href = res.data.response.body.init_point));
+    };
     
     return (
         <>        
@@ -67,9 +85,16 @@ const Carrito = () => {
                 {cartUnificado.map((item, index) => (
                     <div key={index} className="col-span-2 grid grid-cols-6 px-6 mx-6 shadow-md rounded-lg bg-fuchsia-200">
                 <img src={item.objeto.imagenPrincipal} alt="fotoProducto" className="col-span-1 w-12 bg-white my-2 border-2 border-purple-300 justify-self-left" />
-                <div className="col-start-2 col-span-3 place-self-center font-medium">
+                
+                <div class="col-start-2 col-span-3 place-self-center grid grid-rows-2">
+                <div className="grid-row-1 font-medium">
                     {item.objeto.name}
                 </div>
+                <div className="grid-row-2 text-xs">
+                    {item.color}
+                </div>
+                </div>
+                
                 <div className="col-start-5 col-span-1 flex items-center justify-center font-medium ">
                     <p class="text-xs mr-1">Cantidad: </p> {item.cantidad}
                 </div>
@@ -130,7 +155,8 @@ const Carrito = () => {
                 </div>
                 <div class="col-start-3 col-end-4 row-start-4 row-end-4 col-span-1 flex place-self-center">
                 <NavLink to="/catalogo">
-                  <button class="rounded-md place-self-center p-1.5 text-white bg-[#6b086f] hover:bg-[#7c4884]">
+                  <button class="rounded-md place-self-center p-1.5 text-white bg-[#6b086f] hover:bg-[#7c4884]"
+                   >         
                     Agregar articulos
                   </button>
                 </NavLink>
@@ -142,6 +168,245 @@ const Carrito = () => {
     };
 
 export default Carrito;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { useState } from "react";
+// import { useDispatch, useSelector } from "react-redux";
+// import {emptyCartLS} from "../../redux/actions"
+// import { NavLink } from 'react-router-dom';
+
+// const Carrito = () => {
+//     const dispatch = useDispatch();
+    
+//     const cartLS = useSelector(state => state.localCart); //estos son los item en carrito en local/
+//     cartLS.forEach(elem=>{
+//         if (elem !== undefined && elem !== null){
+//         console.log("cartLS",elem.name);        
+//         console.log(elem.amount);
+//     }})
+    
+//     const handleEmptyCart = () =>{
+//         dispatch(emptyCartLS());
+//     }        
+    
+//    /* unificar amount de articulos start*/
+//    const cartUnif = (cart) =>{
+//     const countMap = {};
+//     const validItems = cart.filter(item => item !== undefined && item !== null);
+ 
+//      cart.forEach(item=>{
+//         if (item.id !== undefined && item.id !== null)  
+//         {         
+//          const itemId=item.id;       
+//          if(countMap[itemId]){
+//              countMap[itemId]+=item.amount;
+//          }else{
+//              countMap[itemId]=item.amount;
+//          }             
+//      }});     
+//      const cartUnifRes = Object.keys(countMap).filter((itemId) => {
+//         return cart.find((item) => item.id === itemId) !== undefined;
+//       })
+//         .map((itemId) => {
+//           return {
+//             objeto: cart.find((item) => item.id === itemId),
+//             cantidad: countMap[itemId],
+//           };
+//         });    
+//       return cartUnifRes;
+//     };
+// /* unificar amount de articulos end*/
+
+//     const cartUnificado = cartUnif(cartLS);    
+
+
+//    /* total costo x articulos */
+//    const totalProd = cartUnificado.reduce((total,item)=>total+(item.objeto.precio_venta * item.cantidad),0);
+
+//    /* total de articulos en carrito local*/
+//     const totalArts = cartUnificado.reduce((qty,item)=>qty+(item.cantidad),0);
+    
+//     return (
+//         <>        
+//             <div class="grid grid-cols-3 grid-rows-6 gap-5 mx-8 mt-6">
+//                 {/* columna izquierda detallar productos en carrito */}  
+//                 {cartUnificado && cartUnificado.length > 0 ? (
+//                 <>
+//                 {cartUnificado.map((item, index) => (
+//                     <div key={index} className="col-span-2 grid grid-cols-6 px-6 mx-6 shadow-md rounded-lg bg-fuchsia-200">
+//                 <img src={item.objeto.imagenPrincipal} alt="fotoProducto" className="col-span-1 w-12 bg-white my-2 border-2 border-purple-300 justify-self-left" />
+//                 <div className="col-start-2 col-span-3 place-self-center font-medium">
+//                     {item.objeto.name}
+//                 </div>
+//                 <div className="col-start-5 col-span-1 flex items-center justify-center font-medium ">
+//                     <p class="text-xs mr-1">Cantidad: </p> {item.cantidad}
+//                 </div>
+//                 <div className="col-start-6 col-span-1 flex items-center justify-center font-medium ">
+//                     <p class="text-xs mr-1">Precio: </p>{item.objeto.precio_venta * item.cantidad}
+//                 </div>
+//             </div>
+//         ))}
+//         <div class="col-start-2  flex justify-end h-6">          
+//             <button onClick={handleEmptyCart} class="rounded-md mx-6 px-2 text-gray-400 bg-gray-200 hover:bg-gray-100 font-small">
+//                 Limpiar Carrito
+//             </button>
+//         </div>
+//         </>
+//     ) : (
+//         <div className="col-span-2 grid grid-cols-5 px-6 mx-6 shadow-md rounded-lg bg-fuchsia-200">
+//             <div className="col-start-2 col-span-3 flex items-center justify-center font-medium">
+//             No hay artículos en su carrito
+//             </div>
+//         </div>
+//     )}                     
+
+
+//     {/* columna derecha, total y boton a pasarela */}
+//                 <div class="col-start-3 row-start-1 row-end-4 px-6 mx-6 rounded-lg bg-purple-200">
+
+//                     <div class="grid grid-cols-4 grid-rows-6 m-4">
+//                         <h2 class="col-span-4 row-start-1 place-self-center font-medium">
+//                             Resumen de Compra
+//                         </h2>
+
+//                         <h3 class="col-start-1 col-end-3 row-start-2 place-self-start">
+//                         Arts. ({totalArts || 0})
+//                         </h3>
+//                         <h3 class="col-start-1 col-end-3 row-start-3 place-self-start">
+//                             Envio
+//                         </h3>
+                        
+//                         <h2 class="col-start-1 col-end-3 row-start-5 place-self-start font-bold">
+//                             Total
+//                         </h2>
+
+//                         <h2 class="col-start-4 row-start-2 place-self-start">
+//                         {totalProd || 0}
+//                         </h2>
+//                         <h2 class="row-start-3 col-start-4  place-self-start">
+//                             0
+//                         </h2>
+//                         <h2 class="row-start-5 col-start-4  place-self-start font-bold">
+//                         {totalProd || 0}
+//                         </h2>
+
+//                         <button class="rounded-md row-start-6 place-self-center col-span-4 p-1.5 text-white bg-[#6b086f] hover:bg-[#7c4884]">
+//                             Continuar compra
+//                         </button>
+
+//                     </div>
+//                 </div>
+//                 <div class="col-start-3 col-end-4 row-start-4 row-end-4 col-span-1 flex place-self-center">
+//                 <NavLink to="/catalogo">
+//                   <button class="rounded-md place-self-center p-1.5 text-white bg-[#6b086f] hover:bg-[#7c4884]">
+//                     Agregar articulos
+//                   </button>
+//                 </NavLink>
+//                 </div>
+//             </div>
+//         </>
+//     )
+
+//     };
+
+// export default Carrito;
 
 
 
